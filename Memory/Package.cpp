175534a143bfa869e9fd58b0b884f169f5cfa5dc
Mitchell Willemsen <45740908+oSketchMastah@users.noreply.github.com>
2021-07-05 15:44:16 -0400

    Add files via upload

--- Memory/Package.cpp
+++ Memory/Package.cpp
@@ -1,3 +1,18 @@
+/*
+Copyright 2021 Mitchell Willemsen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 #include <stdio.h>
 #include <stdexcept>
@@ -5,13 +20,7 @@
 #include <unistd.h>
 #include "Package.h"
 #include "XREmbeddor.h"
-/*
-	const char * filename;
-	Embeddor embeddor;
-	void * memory;
-*/
 
-//#define READ_CHUNK_SIZE 65536
 void LoadPackageFromSize(int file, void * memory, unsigned int memlen, const char *filename) {
 	ssize_t nread = read(file, memory, memlen);
 	if (nread <= 0) {
